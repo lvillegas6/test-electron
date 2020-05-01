289aa6f500a6eab822b60454f3c5dcd0828b76f2
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-04-30 20:58:29 -0500

    Code changes

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const { app, BrowserWindow } = require('electron')
 
-app.on('ready', () => {
-    let mainWindow = new BrowserWindow({
-        title: 'Editor',
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+function createWindow () {
+  // Crea la ventana del navegador.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
-    mainWindow.loadURL(`file://${__dirname}/index.html`)
-    mainWindow.webContents.on('will-navigate', (e, url) => {
-        e.preventDefault()
-        mainWindow.webContents.send('open-file', url)
-    })
+  // and load the index.html of the app.
+  win.loadFile('index.html')
 
+  // Abre las herramientas de desarrollo (DevTools).
+  win.webContents.openDevTools()
+
+ 
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Algunas APIs pueden usarse sólo después de que este evento ocurra.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // En macOS es común para las aplicaciones y sus barras de menú
+  // que estén activas hasta que el usuario salga explicitamente con Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
+
+app.on('activate', () => {
+  // En macOS es común volver a crear una ventana en la aplicación cuando el
+  // icono del dock es clicado y no hay otras ventanas abiertas.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. También puedes ponerlos en archivos separados y requerirlos aquí.
